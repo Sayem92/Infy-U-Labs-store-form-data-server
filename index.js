dc8d5fc15d9a593c049a7f96a6926e4f501a37f0
Sayem92 <sayemmiha123@gmail.com>
2023-02-06 15:31:53 +0600

    add delete api

--- index.js
+++ index.js
@@ -56,6 +56,16 @@ async function run() {
             res.send(result)
         });
 
+
+        // delete information---
+        app.delete('/allInformation/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AddInformationCollection.deleteOne(query)
+            res.send(result);
+        });
+
+
         
 
        
