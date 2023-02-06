c96f7b7aa45ddf830293a0566e4e2e9c075b2846
Sayem92 <sayemmiha123@gmail.com>
2023-02-06 17:45:42 +0600

    code organize

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
         });
 
 
-        // get task data for data---
+        // get info data for update---
         app.get('/updateInfor/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
